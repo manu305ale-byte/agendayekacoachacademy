0b9a0704f864a796dc8af89d748470523dfe57f1
manu305ale-byte <manu305ale@gmail.com>
2026-06-06 06:52:01 -0400

    Activar flipbook tipo Heyzine v16

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export const dynamic = 'force-dynamic';
 export default function Home() {
   return (
     <iframe
-      src="/agenda-v15.html?v=15.0"
+      src="/agenda-v16.html?v=16.0"
       title="Agenda Yeka Coach Academy 2027 Flipbook"
       style={{ width: '100vw', height: '100dvh', border: 0, display: 'block' }}
     />
