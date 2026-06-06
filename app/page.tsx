3bfb1d1a92d39598176b7fe9eca973b9a83d3699
manu305ale-byte <manu305ale@gmail.com>
2026-06-06 06:39:20 -0400

    Activar version flipbook vertical

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ export const dynamic = 'force-dynamic';
 export default function Home() {
   return (
     <iframe
-      src="/agenda-v12.html?v=12.0"
-      title="Agenda Yeka Coach Academy 2027"
+      src="/agenda-v13.html?v=13.0"
+      title="Agenda Yeka Coach Academy 2027 Flipbook"
       style={{ width: '100vw', height: '100dvh', border: 0, display: 'block' }}
     />
   );
