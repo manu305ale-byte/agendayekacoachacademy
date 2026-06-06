bfbdefa8c2e7057ab338e33d783d8d5ffd6abae1
manu305ale-byte <manu305ale@gmail.com>
2026-06-06 18:38:26 -0400

    Remove spiral wrapper from home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export const dynamic = 'force-dynamic';
 
 export default function Home() {
-  return <iframe src="/agenda-v30.html" title="Agenda Yeka" style={{ width: '100vw', height: '100dvh', border: 0 }} />;
+  return <iframe src="/agenda-v29.html?v=29.4" title="Agenda Yeka" style={{ width: '100vw', height: '100dvh', border: 0 }} />;
 }
