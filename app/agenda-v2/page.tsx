907d621c94b67f946c5cf4236544c575f38a78b6
manu305ale-byte <manu305ale@gmail.com>
2026-06-05 22:22:31 -0400

    Agregar ruta para agenda interactiva v2

---
+++ app/agenda-v2/page.tsx
@@ -0,0 +1,9 @@
+export default function AgendaV2Page() {
+  return (
+    <iframe
+      src="/agenda-v2.html"
+      style={{ width: '100vw', height: '100vh', border: 0, display: 'block' }}
+      title="Agenda Yeka Coach Academy 2027"
+    />
+  );
+}
