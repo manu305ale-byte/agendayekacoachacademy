b96d21e74bb7cff911c6f37f378d4b1bc7709bad
manu305ale-byte <manu305ale@gmail.com>
2026-06-06 17:43:37 -0400

    Point home to v30

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export const dynamic = 'force-dynamic';
 
 export default function Home() {
-  return <iframe src="/agenda-v29.html?v=29.0" title="Agenda" style={{ width: '100vw', height: '100dvh', border: 0, display: 'block' }} />;
+  return <iframe src="/agenda-v30.html" title="Agenda Yeka" style={{ width: '100vw', height: '100dvh', border: 0 }} />;
 }
