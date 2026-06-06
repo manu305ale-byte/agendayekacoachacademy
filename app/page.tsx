79a2b2022ef132b4b9b93acddec18da6d7e39369
manu305ale-byte <manu305ale@gmail.com>
2026-06-06 08:38:45 -0400

    Use cleaned agenda viewer v17

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ export const dynamic = 'force-dynamic';
 export default function Home() {
   return (
     <iframe
-      src="/agenda-v16.html?v=16.0"
-      title="Agenda Yeka Coach Academy 2027 Flipbook"
+      src="/agenda-v17.html?v=17.0"
+      title="Agenda Yeka Coach Academy 2027"
       style={{ width: '100vw', height: '100dvh', border: 0, display: 'block' }}
     />
   );
