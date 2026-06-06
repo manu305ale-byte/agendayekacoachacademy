a6fd0e8c86be507342f75866fcbd39d1ddebbe5a
manu305ale-byte <manu305ale@gmail.com>
2026-06-06 06:42:51 -0400

    Activar flipbook funcional v15

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export const dynamic = 'force-dynamic';
 export default function Home() {
   return (
     <iframe
-      src="/agenda-v13.html?v=13.0"
+      src="/agenda-v15.html?v=15.0"
       title="Agenda Yeka Coach Academy 2027 Flipbook"
       style={{ width: '100vw', height: '100dvh', border: 0, display: 'block' }}
     />
