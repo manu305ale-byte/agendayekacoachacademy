631eb8fdfaddaaa4224da85217ed068974d49fbe
manu305ale-byte <manu305ale@gmail.com>
2026-06-08 08:28:00 -0400

    Load standalone stable agenda v37

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export const dynamic = 'force-dynamic';
 
 export default function Home() {
-  return <iframe src="/agenda-v36.html" title="Agenda Yeka" style={{ width: '100vw', height: '100dvh', border: 0 }} />;
+  return <iframe src="/agenda-v37.html" title="Agenda Yeka" style={{ width: '100vw', height: '100dvh', border: 0 }} />;
 }
