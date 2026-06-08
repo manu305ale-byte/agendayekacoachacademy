9b58026f176c0890f512bcc6bf29cce3a1e45874
manu305ale-byte <manu305ale@gmail.com>
2026-06-08 11:50:47 -0400

    Load v41 native suitcase agenda

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export const dynamic = 'force-dynamic';
 
 export default function Home() {
-  return <iframe src="/agenda-v40.html" title="Agenda" style={{ width: '100vw', height: '100dvh', border: 0 }} />;
+  return <iframe src="/agenda-v41.html" title="Agenda" style={{ width: '100vw', height: '100dvh', border: 0 }} />;
 }
